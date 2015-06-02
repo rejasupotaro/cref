b42816e7982deb4997e11c2d8933662a0e5b2d08
Kentaro Takiguchi <takiguchi0817@gmail.com>
2015-06-03 05:13:25 +0900

    Enable to receive repository list

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,10 @@ const VERSION: &'static str = "0.0.1";
 docopt!(Args derive Debug, "
 Usage:
   cref
-  cref import <repo>
+  cref import <import-repo>...
   cref list
-  cref update
-  cref delete <repo>
+  cref update [<update-repo>...]
+  cref delete <delete-repo>
   cref (-help | --version)
 
 Options:
@@ -59,13 +59,13 @@ fn run(args: Args) -> SqliteResult<()> {
     create_cref_dir();
 
     if args.cmd_import { // cref import <repo>
-        try!(execute_import(args.arg_repo));
+        try!(execute_import(args.arg_import_repo));
     } else if args.cmd_list { // cref list
         try!(execute_list());
     } else if args.cmd_update { // cref update
-        try!(execute_update());
+        try!(execute_update(args.arg_update_repo));
     } else if args.cmd_delete { // cref delete <repo>
-        try!(execute_delete(args.arg_repo));
+        try!(execute_delete(args.arg_delete_repo));
     } else if args.flag_version { // cref -v
         println!("{}", VERSION);
     } else { // cref
@@ -75,29 +75,52 @@ fn run(args: Args) -> SqliteResult<()> {
     Ok(())
 }
 
-fn execute_import(repository_name: String) -> SqliteResult<()> {
+fn execute_import(repository_names: Vec<String>) -> SqliteResult<()> {
     let mut db = try!(db::Db::new(db_file()));
     let mut github = github::GitHub::new();
-    let commits = github.fetch_commits(&repository_name);
-    db.insert_commits(&repository_name, commits)
+    repository_names.iter().map(|repository_name| {
+            let commits = github.fetch_commits(&repository_name);
+            db.insert_commits(&repository_name, commits);
+        }).collect::<Vec<_>>();
+    Ok(())
 }
 
 fn execute_list() -> SqliteResult<()> {
     let db = try!(db::Db::new(db_file()));
     let repositories = try!(db.select_repositories());
-    repositories.iter().inspect(|repository| {
+    repositories.iter().map(|repository| {
             println!("{:?}", repository);
         }).collect::<Vec<_>>();
     Ok(())
 }
 
-fn execute_update() -> SqliteResult<()> {
-    let mut db = try!(db::Db::new(db_file()));
-    let mut github = github::GitHub::new();
-    try!(db.select_repositories()).iter().map(|repository| {
-            let commits = github.fetch_commits(&repository.name);
-            db.insert_commits(&repository.name, commits);
-        }).collect::<Vec<_>>();
+fn execute_update(repository_names: Vec<String>) -> SqliteResult<()> {
+    let update = |repository_names: Vec<String>| {
+        match db::Db::new(db_file()) {
+            Ok(mut db) => {
+                let mut github = github::GitHub::new();
+
+                repository_names.iter().map(|repository_name| {
+                        let commits = github.fetch_commits(&repository_name);
+                        db.insert_commits(&repository_name, commits);
+                    }).collect::<Vec<_>>();
+            },
+            Err(e) => abort(e.to_string())
+        }
+    };
+
+    match repository_names.len() {
+        0 => {
+            let db = try!(db::Db::new(db_file()));
+            let all_repository_names = try!(db.select_repositories()).iter().map(|repository| {
+                    repository.name.clone()
+                }).collect::<Vec<String>>();
+            update(all_repository_names);
+        },
+        _ => {
+            update(repository_names);
+        }
+    }
     Ok(())
 }
 
